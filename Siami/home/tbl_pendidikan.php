ebd21521834d7083a91de6892b64b5a3df91a16d
Achmad Figo <37265490+Ahmadfigo@users.noreply.github.com>
2021-02-09 11:19:15 +0700

    db

--- Siami/home/tbl_pendidikan.php
+++ Siami/home/tbl_pendidikan.php
@@ -3,3 +3,439 @@
 		<h2>Data Tabel Pendidikan</h2>
 	</div>
 </div>
+
+<?php
+include "../config/database.php";
+
+$query = mysqli_query($db,"SELECT * FROM tbl_kerjasama_pendidikan WHERE id_akred_prodi = 1");
+
+
+?>
+
+<div class="box-body">
+<div class="col-md-12">
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<div class="card">
+		<div class="card-header">
+			<h4 class="card-title"> Simple Table</h4>
+			
+		</div>
+		<div class="card-body">
+			<div class="table-responsive">
+				<table class="table">
+					<thead class=" text-primary">
+						
+	<tr><th>
+		
+			Name
+		
+	</th>
+	
+	<th>
+		
+			Country
+		
+	</th>
+	
+	<th>
+		
+			City
+		
+	</th>
+	
+	<th class="text-right">
+		
+			Salary
+		
+	</th>
+	
+	
+					</tr></thead>
+					<tbody>
+						
+							
+		
+	
+		
+			
+				<tr>
+					
+						
+							
+							
+							<td>
+								
+									Dakota Rice
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Niger
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Oud-Turnhout
+								
+							</td>
+						
+					
+						
+							
+							
+							<td class="text-right">
+								
+									$36,738
+								
+							</td>
+						
+					
+						
+					
+	
+					
+				</tr>
+			
+		
+	
+		
+			
+				<tr>
+					
+						
+							
+							
+							<td>
+								
+									Minerva Hooper
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Curaçao
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Sinaai-Waas
+								
+							</td>
+						
+					
+						
+							
+							
+							<td class="text-right">
+								
+									$23,789
+								
+							</td>
+						
+					
+	
+					
+				</tr>
+			
+		
+	
+		
+			
+				<tr>
+					
+						
+							
+							
+							<td>
+								
+									Sage Rodriguez
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Netherlands
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Baileux
+								
+							</td>
+						
+					
+						
+							
+							
+							<td class="text-right">
+								
+									$56,142
+								
+							</td>
+						
+					
+						
+					
+	
+					
+				</tr>
+			
+		
+	
+		
+			
+				<tr>
+					
+						
+							
+							
+							<td>
+								
+									Philip Chaney
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Korea, South
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Overland Park
+								
+							</td>
+						
+					
+						
+							
+							
+							<td class="text-right">
+								
+									$38,735
+								
+							</td>
+						
+					
+	
+					
+				</tr>
+			
+		
+	
+		
+			
+				<tr>
+					
+						
+							
+							
+							<td>
+								
+									Doris Greene
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Malawi
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Feldkirchen in Kärnten
+								
+							</td>
+						
+					
+						
+							
+							
+							<td class="text-right">
+								
+									$63,542
+								
+							</td>
+						
+					
+						
+					
+	
+					
+				</tr>
+			
+		
+	
+		
+			
+				<tr>
+					
+						
+							
+							
+							<td>
+								
+									Mason Porter
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Chile
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Gloucester
+								
+							</td>
+						
+					
+						
+							
+							
+							<td class="text-right">
+								
+									$78,615
+								
+							</td>
+						
+					
+	
+					
+				</tr>
+			
+		
+	
+		
+			
+				<tr>
+					
+						
+							
+							
+							<td>
+								
+									Jon Porter
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Portugal
+								
+							</td>
+						
+					
+						
+							
+							
+							<td>
+								
+									Gloucester
+								
+							</td>
+						
+					
+						
+							
+							
+							<td class="text-right">
+								
+									$98,615
+								
+							</td>
+						
+					
+						
+					
+	
+					
+				</tr>
+			
+		
+	
+	
+						
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	
+	  </div>
+</div>
