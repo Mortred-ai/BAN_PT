5f4a08934062bff6ecfa604556e674c935377547
baktiarmahardika <baktiarmahardika08@gmail.com>
2021-06-30 13:28:28 +0700

    Modal opacity and Z-Index.

--- Siami/home/tbl_pendidikan.php
+++ Siami/home/tbl_pendidikan.php
@@ -17,16 +17,16 @@
 			</div>
 			<div class="col-sm-2 mt-2">
 				<!-- Button trigger modal -->
-				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-					Tambah Data
+				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">
+					Launch static backdrop modal
 				</button>
 
 				<!-- Modal -->
-				<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-					<div class="modal-dialog">
-						<div class="modal-content">
+				<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+					<div class="modal-dialog modal-xl shadow-lg" style="z-index: 99999999;">
+						<div class="modal-content ">
 							<div class="modal-header">
-								<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+								<h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
 								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 									<span aria-hidden="true">&times;</span>
 								</button>
@@ -36,7 +36,7 @@
 							</div>
 							<div class="modal-footer">
 								<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-								<button type="button" class="btn btn-primary">Save changes</button>
+								<button type="button" class="btn btn-primary">Understood</button>
 							</div>
 						</div>
 					</div>
